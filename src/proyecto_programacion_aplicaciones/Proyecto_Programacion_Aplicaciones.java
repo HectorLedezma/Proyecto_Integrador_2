d892122a2c1b42cf06f9dd55ca996a2786335eb7
Hector Ledezma <73298715+HectorLedezma@users.noreply.github.com>
2022-09-05 21:00:23 -0300

    XD

--- src/proyecto_programacion_aplicaciones/Proyecto_Programacion_Aplicaciones.java
+++ src/proyecto_programacion_aplicaciones/Proyecto_Programacion_Aplicaciones.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Proyecto_Programacion_Aplicaciones {
 
     public static void main(String[] args) {
-        String lenguaje[] = {"Python","Java","C++"};
+        String lenguaje[] = {"Kensel","Vane","Bicho","Hector"};
         //XD
         Random elec = new Random();
         System.out.println(lenguaje[elec.nextInt(lenguaje.length)]);
